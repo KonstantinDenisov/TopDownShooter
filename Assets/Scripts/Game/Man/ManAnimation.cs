4cb5d1b1d12435f7939cba400785b5f7acf47d2e
KonstantinDenisov <omacs@mail.ru>
2022-08-29 22:40:56 +0300

    add first bot mechanics

---
+++ Assets/Scripts/Game/Man/ManAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TDS.Game.Man
+{
+    public class ManAnimation : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        public void PlayShoot()
+        {
+            _animator.SetTrigger("ManShoot");
+        }
+
+        public void SetSpeed(float speed)
+        {
+            _animator.SetFloat("ManSpeed", speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Man/ManAttack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace TDS.Game.Man
+{
+    public class ManAttack : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField] private ManAnimation _manAnimation;
+        [SerializeField] private GameObject _bulletPrefab;
+        [SerializeField] private Transform _bulletSpawnPointTransform;
+        private Transform _cachedTransform;
+        [SerializeField] private Transform _follow;
+
+        #endregion
+
+
+        #region Unity Lifecycle
+
+        private void Awake()
+        {
+            _cachedTransform = transform;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(FireRoutine());
+        }
+
+        private void Update()
+        {
+            Rotate();
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        private void Attack()
+        {
+            _manAnimation.PlayShoot();
+            Instantiate(_bulletPrefab, _bulletSpawnPointTransform.position, _cachedTransform.rotation);
+        }
+        
+        private void Rotate()
+        {
+            Vector3 direction = _follow.position - _cachedTransform.position;
+            _cachedTransform.up = direction;
+        }
+
+       
+        
+        private IEnumerator FireRoutine()
+        {
+            while (true)
+            {
+                Attack();
+
+                yield return new WaitForSeconds(4); 
+            }
+        }
+
+        #endregion
+
+       
+    }
+}
